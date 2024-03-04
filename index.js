981915ffd65b422b91cb9396ccce72a2bf832a3a
Brian <turtle40965@gmail.com>
2024-03-04 13:18:21 -0800

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+// for ([initialization]; [condition]; [iteration]) {
+//   [loop body]
+// }
+
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+  const messages = [];
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    messages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    debugger;
+  }
+
+  return messages;
+}
+
+writeCards(cards);
+
+function countDown(number){
+  let countdown = 10;
+while (countdown > -1) {
+  console.log(countdown--);
+}
+}
